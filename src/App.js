2b05092c8d81ebd661e92decbcd32ec8fefc8ce8
Colin Heng <colinheng@gmail.com>
2023-08-23 11:00:06 +0800

    Completed custom hooks 1 and 2

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import UseRefExample2 from "./components/UseRefExample2"
 import UseRefExample3 from "./components/UseRefExample3"
 import UseMemoExample from "./components/UseMemoExample"
 import UseCallbackExample from "./components/UseCallbackExample"
+import CustomHookExample1 from "./components/CustomHookExample1"
+import CustomHookExample2 from "./components/CustomHookExample2"
 
 function App() {
   return (
     <div className="container mt-5">
-      <UseRefExample1 />
-      <UseRefExample2 />
-      <UseRefExample3 />
-      <UseMemoExample />
-      <UseCallbackExample />
+      <p>7.<CustomHookExample2 /></p>
+      <p>6.<CustomHookExample1 /></p>
+      <p>5.<UseCallbackExample /></p>
+      <p>4.<UseMemoExample /></p>
+      <p>3.<UseRefExample3 /></p>
+      <p>2.<UseRefExample2 /></p>
+      <p>1.<UseRefExample1 /></p>
+
     </div>
   )
 }
